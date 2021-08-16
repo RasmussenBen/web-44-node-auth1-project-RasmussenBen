75d71116a670bb84e980a16370c5db3f521375fd
Ben Rasmussen <rasmussen.ben32@gmail.com>
2021-08-16 15:47:42 -0500

    middleware

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,49 +1,66 @@
-/*
-  If the user does not have a session saved in the server
+const User = require('../users/users-model')
 
-  status 401
-  {
-    "message": "You shall not pass!"
+function restricted(req, res, next) {
+  if (req.session.user) {
+    next()
   }
-*/
-function restricted() {
-
+  else {
+    next({
+      status:401, message: 'You shall not pass!'
+    })
+  }
 }
 
-/*
-  If the username in req.body already exists in the database
+async function checkUsernameFree(req, res, next) {
+  try {
+  const users = await User.findBy({
+    username: req.body.username
+  })
 
-  status 422
-  {
-    "message": "Username taken"
+  if (!users.length) {
+    next()
   }
-*/
-function checkUsernameFree() {
-
+  else {
+    next({ status: 422, message: 'Username taken' })
+  }}
+  catch (err) {
+    next(err)
+  }
+}
+  
+async function checkUsernameExists(req, res, next) {
+  try{
+    const users = await User.findBy({
+      username: req.body.username
+    })
+    if (users.length) {
+      req.user = users[0]
+    }
+    else {
+      next({
+        status: 401, message: 'Invalid credentials'
+      })
+    }
+  }
+  catch (err) {
+    next(err)
+  }
 }
 
-/*
-  If the username in req.body does NOT exist in the database
-
-  status 401
-  {
-    "message": "Invalid credentials"
+function checkPasswordLength(req, res, next) {
+  if (!req.body.password || req.body.password.length < 3) {
+    next({
+      status:422, message: 'Password must be longer than 3 characters'
+    })
   }
-*/
-function checkUsernameExists() {
-
+  else {
+    next()
+  }
 }
 
-/*
-  If password is missing from req.body, or if it's 3 chars or shorter
-
-  status 422
-  {
-    "message": "Password must be longer than 3 chars"
-  }
-*/
-function checkPasswordLength() {
-
+module.exports = {
+  restricted,
+  checkUsernameFree,
+  checkUsernameExists,
+  checkPasswordLength
 }
-
-// Don't forget to add these to the `exports` object so they can be required in other modules
