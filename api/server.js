bfdc3af97772c349ccb0fe07351fff0326e65033
Ben Rasmussen <rasmussen.ben32@gmail.com>
2021-08-16 16:08:04 -0500

    server wip

--- api/server.js
+++ api/server.js
@@ -2,6 +2,14 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 
+const authRouter = require('./auth/auth-router')
+const usersRouter = require('./users/users-router')
+const session = require('express-session')
+const Store = require('connect-session-knex')(session)
+const knex = require('../data/db-config')
+
+const server = express();
+
 /**
   Do what needs to be done to support sessions with the `express-session` package!
   To respect users' privacy, do NOT send them a cookie unless they log in.
@@ -13,13 +21,14 @@ const cors = require("cors");
 
   The session can be persisted in memory (would not be adecuate for production)
   or you can use a session store like `connect-session-knex`.
- */
-
-const server = express();
+*/
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+
+server.use('/api/auth', authRouter)
+server.use('/api/users', usersRouter)
 
 server.get("/", (req, res) => {
   res.json({ api: "up" });
@@ -31,5 +40,24 @@ server.use((err, req, res, next) => { // eslint-disable-line
     stack: err.stack,
   });
 });
+
+server.use(session({
+  name: 'test',
+  secret: 'secret test',
+  saveUninitialized: false,
+  resave: false,
+  store: new Store({
+    knex,
+    createtable: true,
+    clearInterval: 1000 * 60 * 60,
+    tablename:'session',
+    sidfieldname:'sid'
+  }),
+  cookie:{
+    maxAge:1000 * 60 * 60,
+    secure: false,
+    httpOnly: true
+  }
+}))
 
 module.exports = server;
